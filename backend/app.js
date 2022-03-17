d8d7d958a8c0fa91a66fa90389a38a8212a2cffb
Brigitte Tricot-Censier <brigittetc.pub@hotmail.fr>
2022-03-17 19:39:40 +0100

    app.js: ajout middleware.

--- backend/app.js
+++ backend/app.js
@@ -10,9 +10,28 @@ const express = require("Express");
 // application
 const app = express();
 
+// Premier middleware
+//next permet d'envoyer la réponse et de terminer la requete
+app.use((req, res, next) => {
+  console.log("requete reçue");
+  next();
+});
+
+//middleware suivant
+app.use((req, res, next) => {
+  res.status(201);
+  next();
+});
+
 //Traitement de la requete
-app.use((req, res) => {
+app.use((req, res, next) => {
   res.json({ message: "requete bien reçue" });
+  next();
+});
+
+//middleware suivant: pas de next car c'est le dernier
+app.use((req, res) => {
+  console.log("réponse envoyée avec succes");
 });
 
 //export de la fonction pour qu'on puisse y acceder depuis les autres fichiers du projet
