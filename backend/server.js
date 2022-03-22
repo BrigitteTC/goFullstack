f4db5fb9d061170e2d3f3e5d2bff689922d2444a
Brigitte Tricot-Censier <brigittetc.pub@hotmail.fr>
2022-03-22 17:29:04 +0100

    server.js: ajout commentaires suite modif "express"

--- backend/server.js
+++ backend/server.js
@@ -28,6 +28,9 @@
 const http = require("http");
 const app = require("./app");
 
+
+//fonction normalizePort renvoie un port valide, qu'il soit fourni sous 
+//la forme d'un numéro ou d'une chaîne;
 const normalizePort = (val) => {
   const port = parseInt(val, 10);
 
@@ -41,6 +44,10 @@ const normalizePort = (val) => {
 };
 const port = normalizePort(process.env.PORT || "3000");
 app.set("port", port);
+
+
+//la fonction errorHandler  recherche les différentes erreurs 
+//et les gère de manière appropriée.Elle est ensuite enregistrée dans le serveur;
 
 const errorHandler = (error) => {
   if (error.syscall !== "listen") {
@@ -66,6 +73,10 @@ const errorHandler = (error) => {
 const server = http.createServer(app);
 
 server.on("error", errorHandler);
+
+//un écouteur d'évènements est également enregistré, 
+//consignant le port ou le canal nommé sur lequel le serveur s'exécute dans la console.
+
 server.on("listening", () => {
   const address = server.address();
   const bind = typeof address === "string" ? "pipe " + address : "port " + port;
