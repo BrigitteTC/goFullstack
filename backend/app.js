19960ee1e5c2dcfb1d253f4747f30126a4fcddd1
Brigitte Tricot-Censier <brigittetc.pub@hotmail.fr>
2022-03-22 17:42:09 +0100

    app.js: nouvelle version du middleware.

--- backend/app.js
+++ backend/app.js
@@ -10,6 +10,7 @@ const express = require("Express");
 // application
 const app = express();
 
+/* premiers middleware abandonnés pour la nouvelle version du 22 mars 2022
 // Premier middleware
 //next permet d'envoyer la réponse et de terminer la requete
 app.use((req, res, next) => {
@@ -32,6 +33,34 @@ app.use((req, res, next) => {
 //middleware suivant: pas de next car c'est le dernier
 app.use((req, res) => {
   console.log("réponse envoyée avec succes");
+});
+
+*/
+
+//Nouvelle version du middleware du 22 mars 2022
+
+app.use("/api/stuff", (req, res, next) => {
+  const stuff = [
+    {
+      _id: "oeihfzeoi",
+      title: "Mon premier objet",
+      description: "Les infos de mon premier objet",
+      imageUrl:
+        "https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg",
+      price: 4900,
+      userId: "qsomihvqios",
+    },
+    {
+      _id: "oeihfzeomoihi",
+      title: "Mon deuxième objet",
+      description: "Les infos de mon deuxième objet",
+      imageUrl:
+        "https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg",
+      price: 2900,
+      userId: "qsomihvqios",
+    },
+  ];
+  res.status(200).json(stuff);
 });
 
 //export de la fonction pour qu'on puisse y acceder depuis les autres fichiers du projet
