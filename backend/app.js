a76076af8bcfcf5104cc96ffd9f5f1ec7321bccc
Brigitte Tricot-Censier <brigittetc.pub@hotmail.fr>
2022-03-25 16:02:15 +0100

    app.js:  suppression déclarations inutiles.

--- backend/app.js
+++ backend/app.js
@@ -36,19 +36,8 @@ remplacé par
 --------------------------------------------------------------------
 */
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri =
   "mongodb+srv://BTCUser1:BTCUser1Passwd@cluster0.wupp6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverApi: ServerApiVersion.v1,
-});
-client.connect((err) => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
 
 mongoose
   .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
