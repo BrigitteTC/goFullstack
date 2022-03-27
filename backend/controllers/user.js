5a3d5a3605cc8b1536115347c62ef28c5559c539
Brigitte Tricot-Censier <brigittetc.pub@hotmail.fr>
2022-03-27 17:28:47 +0200

    user.js: fonction login

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -33,4 +33,27 @@ exports.signup = (req, res, next) => {
 };
 
 // ft login pour connecter les utilisateurs.
-exports.login = (req, res, next) => {};
+exports.login = (req, res, next) => {
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      if (!user) {
+        return res.status(401).json({ error: "utilisateur inconnu" });
+      }
+
+      //bcrypt pour vérifier le mot de passe envoyé par l'utilisateur avec le hash enregistré
+      //Si correct renvoi du TOKEN au frontrnd
+      bcrypt
+        .compare(req.body.password, user.password)
+        .then((valid) => {
+          if (!valid) {
+            return res.status(401).json({ error: "Mot de passe incorrect" });
+          }
+          res.status(200).json({
+            userId: user._id,
+            token: "TOKEN",
+          });
+        })
+        .catch((error) => res.status(500).json({ error })); //500 = error serveur
+    })
+    .catch((error) => res.status(500).json({ error })); //500 = error serveur
+};
