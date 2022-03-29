df25b28a50f8fd454c95f714024bfa63fe259ff1
Brigitte Tricot-Censier <brigittetc.pub@hotmail.fr>
2022-03-29 18:05:55 +0200

    user.js: correction codes d'erreur.

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -52,7 +52,7 @@ exports.login = (req, res, next) => {
         .compare(req.body.password, user.password)
         .then((valid) => {
           if (!valid) {
-            return res.status(402).json({ error: "Mot de passe incorrect" });
+            return res.status(401).json({ error: "Mot de passe incorrect" });
           }
           res.status(200).json({
             userId: user._id,
@@ -68,7 +68,7 @@ exports.login = (req, res, next) => {
             ),
           });
         })
-        .catch((error) => res.status(501).json({ error })); //500 = error serveur
+        .catch((error) => res.status(500).json({ error })); //500 = error serveur
     })
-    .catch((error) => res.status(502).json({ error })); //500 = error serveur
+    .catch((error) => res.status(500).json({ error })); //500 = error serveur
 };
