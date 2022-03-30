3a67bb5d046ce58ffa7f44ace5c51dcd8de97ccf
Brigitte Tricot-Censier <brigittetc.pub@hotmail.fr>
2022-03-30 13:42:42 +0200

    routes/stuff.js: ajout import de multer et ajout multer dans route POST.

--- backend/routes/stuff.js
+++ backend/routes/stuff.js
@@ -11,13 +11,16 @@ const router = express.Router();
 
 const stuffCtrl = require("../controllers/stuff");
 const auth = require("../middleware/auth");
+const multer = require("../middleware/multer-config");
 
 //auth dans chaque route permet de vérifier l'authentification et de la protéger
 
-router.get("/", auth, stuffCtrl.getAllStuff);
-router.post("/", auth, stuffCtrl.createThing);
-router.get("/:id", auth, stuffCtrl.getOneThing);
+router.post("/", auth, multer, stuffCtrl.createThing);
 router.put("/:id", auth, stuffCtrl.modifyThing);
+
+router.get("/", auth, stuffCtrl.getAllStuff);
+router.get("/:id", auth, stuffCtrl.getOneThing);
+
 router.delete("/:id", auth, stuffCtrl.deleteThing);
 
 /*
