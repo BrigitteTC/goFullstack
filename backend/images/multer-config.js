53887b6c1e5e8bb170bf04d9c21aa54e60d0f374
Brigitte Tricot-Censier <brigittetc.pub@hotmail.fr>
2022-03-30 08:01:55 +0200

    Ajout images/multer-config.js.

---
+++ backend/images/multer-config.js
@@ -0,0 +1,30 @@
+/*-----------------------------------------------------------------------------
+multer-config.js
+Date de création: 30/03/2022
+Auteur: BTC
+
+Gère les fichiers et permet leur téléchargement
+--------------------------------------------------------------------------------
+*/
+
+const multer = require("multer");
+
+const MIME_TYPES = {
+  "image/jpg": "jpg",
+  "image/jpeg": "jpg",
+  "image/png": "png",
+};
+
+//Fonction permet le stockage sur le disque
+const storage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    callback(null, "images");
+  },
+  filename: (req, file, callback) => {
+    const name = file.originalname.split(" ").join("_");
+    const extension = MIME_TYPES[file.mimetype];
+    callback(null, name + Date.now() + "." + extension);
+  },
+});
+
+module.exports = multer({ storage: storage }).single("image");
